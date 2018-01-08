f6c1a6a2d6613c0be74b0f32530cffc77c4be451
Damien Clark <damo.clarky@gmail.com>
2018-01-08 20:33:55 +1000

    Implemented tests in content-type.js

--- test/content-type.js
+++ test/content-type.js
@@ -2,7 +2,8 @@ var setup = require('./support/setup') ;
 var uri = setup.uri ;
 
 var assert = require('assert') ;
-var request = require('superagent') ;
+var request = require('../') ;
+var superagent = require('superagent') ;
 
 /*eslint no-undef:0*/
 /*eslint no-unused-vars:0*/
@@ -10,24 +11,26 @@ describe('req.set("Content-Type", contentType)', function(){
 	this.timeout(20000) ;
 
 	it('should work with just the contentType component', function(done){
-		request
-		.post(uri + '/echo')
-		.set('Content-Type', 'application/json')
-		.send({ name: 'tobi' })
-		.end(function(err, res){
-			assert(!err) ;
-			done() ;
+		request(superagent, {
+			post:uri + '/echo',
+			set: ['Content-Type', 'application/json'],
+			send:{ name: 'tobi' },
+			end:function(err, res){
+				assert(!err) ;
+				done() ;
+			}
 		}) ;
 	}) ;
 
 	it('should work with the charset component', function(done){
-		request
-		.post(uri + '/echo')
-		.set('Content-Type', 'application/json; charset=utf-8')
-		.send({ name: 'tobi' })
-		.end(function(err, res){
-			assert(!err) ;
-			done() ;
+		request(superagent, {
+			post:uri + '/echo',
+			set: ['Content-Type', 'application/json; charset=utf-8'],
+			send:{ name: 'tobi' },
+			end:function(err, res){
+				assert(!err) ;
+				done() ;
+			}
 		}) ;
 	}) ;
 
