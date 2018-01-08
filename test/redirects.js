5d4f56732bc09d5f3e206d6f4ee089df59e78a43
Damien Clark <damo.clarky@gmail.com>
2018-01-08 21:06:58 +1000

    Implemented tests in redirects.js

--- test/redirects.js
+++ test/redirects.js
@@ -3,7 +3,8 @@ var base = setup.uri ;
 var isMSIE = !setup.NODE && /Trident\//.test(navigator.userAgent) ;
 
 var assert = require('assert') ;
-var request = require('superagent') ;
+var request = require('../') ;
+var superagent = require('superagent') ;
 
 /*eslint no-undef:0*/
 /*eslint no-unused-vars:0*/
@@ -11,59 +12,63 @@ describe('request', function(){
 	this.timeout(20000) ;
 	describe('on redirect', function(){
 		it('should retain header fields', function(done){
-			request
-			.get(base + '/header')
-			.set('X-Foo', 'bar')
-			.end(function(err, res){
-				try {
-					assert(res.body) ;
-					res.body.should.have.property('x-foo', 'bar') ;
-					done() ;
-				}
-				catch(err) {
-					done(err) ;
+			request(superagent, {
+				get: base + '/header',
+				set: ['X-Foo', 'bar'],
+				end: function(err, res){
+					try {
+						assert(res.body) ;
+						res.body.should.have.property('x-foo', 'bar') ;
+						done() ;
+					}
+					catch(err) {
+						done(err) ;
+					}
 				}
 			}) ;
 		}) ;
 
 		it('should preserve timeout across redirects', function(done){
-			request
-			.get(base + '/movies/random')
-			.timeout(250)
-			.end(function(err, res){
-				try {
-					assert(err instanceof Error, 'expected an error') ;
-					err.should.have.property('timeout', 250) ;
-					done() ;
-				}
-				catch(err) {
-					done(err) ;
+			request(superagent, {
+				get: base + '/movies/random',
+				timeout: 250,
+				end: function(err, res){
+					try {
+						assert(err instanceof Error, 'expected an error') ;
+						err.should.have.property('timeout', 250) ;
+						done() ;
+					}
+					catch(err) {
+						done(err) ;
+					}
 				}
 			}) ;
 		}) ;
 
 		it('should successfully redirect after retry on error', function(done){
 			var id = Math.random() * 1000000 * Date.now() ;
-			request
-			.get(base + '/error/redirect/' + id)
-			.retry(2)
-			.end(function(err, res){
-				assert(res.ok, 'response should be ok') ;
-				assert(res.text, 'first movie page') ;
-				done() ;
+			request(superagent, {
+				get: base + '/error/redirect/' + id,
+				retry: 2,
+				end: function(err, res){
+					assert(res.ok, 'response should be ok') ;
+					assert(res.text, 'first movie page') ;
+					done() ;
+				}
 			}) ;
 		}) ;
 
 		it('should preserve retries across redirects', function(done) {
 			var id = Math.random() * 1000000 * Date.now() ;
-			request
-			.get(base + '/error/redirect-error' + id)
-			.retry(2)
-			.end(function(err, res){
-				assert(err, 'expected an error') ;
-				assert.equal(2, err.retries, 'expected an error with .retries') ;
-				assert.equal(500, err.status, 'expected an error status of 500') ;
-				done() ;
+			request(superagent, {
+				get: base + '/error/redirect-error' + id,
+				retry: 2,
+				end: function(err, res){
+					assert(err, 'expected an error') ;
+					assert.equal(2, err.retries, 'expected an error with .retries') ;
+					assert.equal(500, err.status, 'expected an error status of 500') ;
+					done() ;
+				}
 			}) ;
 
 		}) ;
@@ -71,20 +76,21 @@ describe('request', function(){
 
 	describe('on 303', function(){
 		it('should redirect with same method', function(done){
-			request
-			.put(base + '/redirect-303')
-			.send({msg: 'hello'})
-			.redirects(1)
-			.on('redirect', function(res) {
-				res.headers.location.should.equal('/reply-method') ;
-			})
-			.end(function(err, res){
-				if (err) {
-					done(err) ;
-					return ;
+			request(superagent, {
+				put: base + '/redirect-303',
+				send: {msg: 'hello'},
+				redirects: 1,
+				on: ['redirect', function(res) {
+					res.headers.location.should.equal('/reply-method') ;
+				}],
+				end: function(err, res){
+					if (err) {
+						done(err) ;
+						return ;
+					}
+					res.text.should.equal('method=get') ;
+					done() ;
 				}
-				res.text.should.equal('method=get') ;
-				done() ;
 			}) ;
 		}) ;
 	}) ;
@@ -93,20 +99,21 @@ describe('request', function(){
 		it('should redirect with same method', function(done){
 			if (isMSIE) return done() ; // IE9 broken
 
-			request
-			.put(base + '/redirect-307')
-			.send({msg: 'hello'})
-			.redirects(1)
-			.on('redirect', function(res) {
-				res.headers.location.should.equal('/reply-method') ;
-			})
-			.end(function(err, res){
-				if (err) {
-					done(err) ;
-					return ;
+			request(superagent, {
+				put: base + '/redirect-307',
+				send: {msg: 'hello'},
+				redirects: 1,
+				on: ['redirect', function(res) {
+					res.headers.location.should.equal('/reply-method') ;
+				}],
+				end: function(err, res){
+					if (err) {
+						done(err) ;
+						return ;
+					}
+					res.text.should.equal('method=put') ;
+					done() ;
 				}
-				res.text.should.equal('method=put') ;
-				done() ;
 			}) ;
 		}) ;
 	}) ;
@@ -115,20 +122,21 @@ describe('request', function(){
 		it('should redirect with same method', function(done){
 			if (isMSIE) return done() ; // IE9 broken
 
-			request
-			.put(base + '/redirect-308')
-			.send({msg: 'hello'})
-			.redirects(1)
-			.on('redirect', function(res) {
-				res.headers.location.should.equal('/reply-method') ;
-			})
-			.end(function(err, res){
-				if (err) {
-					done(err) ;
-					return ;
+			request(superagent, {
+				put: base + '/redirect-308',
+				send: {msg: 'hello'},
+				redirects: 1,
+				on: ['redirect', function(res) {
+					res.headers.location.should.equal('/reply-method') ;
+				}],
+				end: function(err, res){
+					if (err) {
+						done(err) ;
+						return ;
+					}
+					res.text.should.equal('method=put') ;
+					done() ;
 				}
-				res.text.should.equal('method=put') ;
-				done() ;
 			}) ;
 		}) ;
 	}) ;
