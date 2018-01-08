9f5e10fa3fba552ad040f4f6f451e4332fabb00a
Damien Clark <damo.clarky@gmail.com>
2018-01-08 20:34:22 +1000

    Implemented tests in form.js

--- test/form.js
+++ test/form.js
@@ -1,7 +1,8 @@
 var setup = require('./support/setup') ;
 var base = setup.uri ;
 var should = require('should') ;
-var request = require('superagent') ;
+var request = require('../') ;
+var superagent = require('superagent') ;
 
 var assert = require('assert') ;
 if (!assert.deepStrictEqual) assert.deepStrictEqual = assert.deepEqual ;
@@ -13,29 +14,30 @@ var formDataSupported = setup.NODE || 'undefined' !== FormData ;
 describe('req.send(Object) as "form"', function(){
 	describe('with req.type() set to form', function(){
 		it('should send x-www-form-urlencoded data', function(done){
-			request
-			.post(base + '/echo')
-			.type('form')
-			.send({ name: 'tobi' })
-			.end(function(err, res){
-				res.header['content-type'].should.equal('application/x-www-form-urlencoded') ;
-				res.text.should.equal('name=tobi') ;
-				done() ;
+			request(superagent, {
+				post:base + '/echo',
+				type:'form',
+				send:{ name: 'tobi' },
+				end:function(err, res){
+					res.header['content-type'].should.equal('application/x-www-form-urlencoded') ;
+					res.text.should.equal('name=tobi') ;
+					done() ;
+				}
 			}) ;
 		}) ;
 	}) ;
 
 	describe('when called several times', function(){
 		it('should merge the objects', function(done){
-			request
-			.post(base + '/echo')
-			.type('form')
-			.send({ name: { first: 'tobi', last: 'holowaychuk' } })
-			.send({ age: '1' })
-			.end(function(err, res){
-				res.header['content-type'].should.equal('application/x-www-form-urlencoded') ;
-				res.text.should.equal('name%5Bfirst%5D=tobi&name%5Blast%5D=holowaychuk&age=1') ;
-				done() ;
+			request(superagent, {
+				post:base + '/echo',
+				type:'form',
+				send: [ [{ name: { first: 'tobi', last: 'holowaychuk' } }], [{ age: '1' }] ],
+				end:function(err, res){
+					res.header['content-type'].should.equal('application/x-www-form-urlencoded') ;
+					res.text.should.equal('name%5Bfirst%5D=tobi&name%5Blast%5D=holowaychuk&age=1') ;
+					done() ;
+				}
 			}) ;
 		}) ;
 	}) ;
@@ -43,11 +45,12 @@ describe('req.send(Object) as "form"', function(){
 
 describe('req.attach', function(){
 	it('ignores null file', function(done){
-		request
-		.post('/echo')
-		.attach('image', null)
-		.end(function(err, res){
-			done() ;
+		request(superagent, {
+			post:'/echo',
+			attach: ['image', null],
+			end:function(err, res){
+				done() ;
+			}
 		}) ;
 	}) ;
 }) ;
@@ -58,14 +61,14 @@ describe('req.field', function(){
 			return done() ;
     
 
-		request
-		.post(base + '/formecho')
-		.field('bools', true)
-		.field('strings', 'true')
-		.end(function(err, res){
-			assert.ifError(err) ;
-			assert.deepStrictEqual(res.body, {bools:'true', strings:'true'}) ;
-			done() ;
+		request(superagent, {
+			post:base + '/formecho',
+			field: [ ['bools', true], ['strings', 'true'] ],
+			end:function(err, res){
+				assert.ifError(err) ;
+				assert.deepStrictEqual(res.body, {bools:'true', strings:'true'}) ;
+				done() ;
+			}
 		}) ;
 	}) ;
 
@@ -74,13 +77,14 @@ describe('req.field', function(){
 			return done() ;
     
 
-		request
-		.post(base + '/formecho')
-		.field({bools: true, strings: 'true'})
-		.end(function(err, res){
-			assert.ifError(err) ;
-			assert.deepStrictEqual(res.body, {bools:'true', strings:'true'}) ;
-			done() ;
+		request(superagent, {
+			post:base + '/formecho',
+			field:{bools: true, strings: 'true'},
+			end:function(err, res){
+				assert.ifError(err) ;
+				assert.deepStrictEqual(res.body, {bools:'true', strings:'true'}) ;
+				done() ;
+			}
 		}) ;
 	}) ;
 
@@ -89,13 +93,14 @@ describe('req.field', function(){
 			return done() ;
     
 
-		request
-		.post(base + '/formecho')
-		.field({numbers: [1, 2, 3]})
-		.end(function(err, res){
-			assert.ifError(err) ;
-			assert.deepStrictEqual(res.body, {numbers:['1', '2', '3']}) ;
-			done() ;
+		request(superagent, {
+			post:base + '/formecho',
+			field:{numbers: [1, 2, 3]},
+			end:function(err, res){
+				assert.ifError(err) ;
+				assert.deepStrictEqual(res.body, {numbers:['1', '2', '3']}) ;
+				done() ;
+			}
 		}) ;
 	}) ;
 
@@ -104,27 +109,30 @@ describe('req.field', function(){
 			return done() ;
     
 
-		request
-		.post(base + '/formecho')
-		.field('letters', ['a', 'b', 'c'])
-		.end(function(err, res){
-			assert.ifError(err) ;
-			assert.deepStrictEqual(res.body, {letters: ['a', 'b', 'c']}) ;
-			done() ;
+		request(superagent, {
+			post:base + '/formecho',
+			field: [ 'letters', ['a', 'b', 'c'] ],
+			end:function(err, res){
+				assert.ifError(err) ;
+				assert.deepStrictEqual(res.body, {letters: ['a', 'b', 'c']}) ;
+				done() ;
+			}
 		}) ;
 	}) ;
 
 	it('throw when empty', function(){
 		should.throws(function(){
-			request
-			.post(base + '/echo')
-			.field() ;
+			request(superagent, {
+				post:base + '/echo',
+				field: undefined
+			}) ;
 		}, /name/) ;
 
 		should.throws(function(){
-			request
-			.post(base + '/echo')
-			.field('name') ;
+			request(superagent, {
+				post:base + '/echo',
+				field: 'name'
+			}) ;
 		}, /val/) ;
 	}) ;
 }) ;
